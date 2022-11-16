00504f674b7326423f17fdf7c4face606d7b7af6
kornspb <kornwork24@mail.ru>
2022-11-16 20:59:55 +0300

    rounded text on main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,62 @@
-/** Анимация на главной JRO. */
-const mainPage = document.getElementById('main-page');
+/** Переменные для проверки медиа-запроса. */
+const mobileMedia = window.matchMedia("(max-width: 767px)")
+const desktopMedia = window.matchMedia("(min-width: 768px)")
 
-window.addEventListener('DOMContentLoaded', function () {
-    console.log(mainPage)
+//TODO Перенести в отдельный файл и подгружать только на главной странице.
+/** Функция создания анимации на главной странице. */
+function createMainAnimation() {
+    /** Враппер главной страницы. */
+    const mainPage = document.getElementById('main-page');
+
+    /** Если главная страница, то подгружаем анимацию JRO. */
     if (mainPage) {
-        console.log('here')
-        new rive.Rive({
-            src: "./assets/riv/jro-new.riv",
-            // Or the path to a local Rive asset
-            // src: './example.riv',
-            canvas: document.getElementById("animation-jro"),
-            autoplay: true
-        });
+        /** Анимация на главной JRO. */
+        if (desktopMedia.matches) {
+            /** Десктоп версия. */
+            new rive.Rive({
+                src: "./assets/riv/jro-desktop.riv",
+                // Or the path to a local Rive asset
+                // src: './example.riv',
+                canvas: document.getElementById("animation-jro-desktop"),
+                autoplay: true
+            });
+        }else{
+            /** Мобильная версия. */
+            new rive.Rive({
+                src: "./assets/riv/jro-mobile.riv",
+                // Or the path to a local Rive asset
+                // src: './example.riv',
+                canvas: document.getElementById("animation-jro-mobile"),
+                autoplay: true
+            });
+        }
     }
-})
+}
 
+//TODO Перенести в отдельный файл и подгружать только на главной странице.
+window.addEventListener('DOMContentLoaded', createMainAnimation);
+
+//TODO Перенести в отдельный файл и подгружать только на главной странице.
+// window.addEventListener('resize', createMainAnimation);
+
+/** Скрипт закругления текста. */
+/*const circleText = document.querySelectorAll('.circle-text .text');
+circleText.forEach(textBlock => {
+    const words = textBlock.textContent;
+
+    textBlock.textContent = '';
+
+    const symbolsArr = words.split('');
+    const symbolsQuantity = symbolsArr.length;
+
+    const circleStep = 360 / symbolsQuantity;
+
+    for (let i = 0; i < symbolsQuantity; i++) {
+        textBlock.innerHTML += `<span style="transform: rotate(${circleStep*i}deg)">${symbolsArr[i]}</span>`;
+    }
+});*/
+
+/** Какой-то Swiper */
 new Swiper('.swiper', {
     spaceBetween: 30,
     slidersPerView: 3,
